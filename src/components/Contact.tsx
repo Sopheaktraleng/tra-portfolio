13a9300e7053792be9927fe72aaafda8dc0fe078
Leng Sopheaktra <sopheaktraleng4@gmail.com>
2025-01-09 15:25:22 +0700

    feat: add contact layout

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,12 +1,48 @@
 const Contact = () => {
     return (
-        <section className="py-16">
+        <section className="py-16 flex justify-center items-center">
             <div className="max-w-6xl mx-auto px-6">
                 <h2 className="text-4xl font-bold mb-4">Contact</h2>
-                <p className="text-lg mb-8">
+                <p className="text-lg mb-8 text-center">
                     Feel free to reach out to me for any questions or
                     opportunities!
                 </p>
+                <form
+                    action=""
+                    className="flex flex-col gap-4 rounded-lg p-10 w-[450px] md:w-[600px] border border-[#33353F]"
+                >
+                    <label
+                        htmlFor="email"
+                        className="font-bold text-lg text-start"
+                    >
+                        Email Me 🚀
+                    </label>
+                    <input
+                        name="email"
+                        type="email"
+                        id="email"
+                        required
+                        placeholder="Email"
+                        className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                    />
+                    <input
+                        name="subject"
+                        type="text"
+                        id="subject"
+                        placeholder="Subject"
+                        className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                    />
+                    <textarea
+                        name="message"
+                        id="message"
+                        placeholder="Message"
+                        className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                    />
+
+                    <button className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full">
+                        Send Message
+                    </button>
+                </form>
             </div>
         </section>
     );
