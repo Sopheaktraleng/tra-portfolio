bed6dea4d48daf444e4db6cf284feee364368638
Sopheaktraleng <sopheaktraleng4@gmail.com>
2025-07-16 15:57:08 +0700

    feat: implement light mode and dark mode base on host display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-//import StarsCanvas from "@/components/animations/StarBackground";
+import ThemeProvider from "@/components/ThemeProvider";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({
     variable: "--font-geist-mono",
     subsets: ["latin"],
@@ -20,16 +16,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
     children,
-}: Readonly<{
+}: {
     children: React.ReactNode;
-}>) {
+}) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                {/* <StarsCanvas /> */}
-                {children}
+                <ThemeProvider>{children}</ThemeProvider>
             </body>
         </html>
     );
