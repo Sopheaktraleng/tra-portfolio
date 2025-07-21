06c5c21f34e0829b58079023c63f56976fc1bb3c
Sopheaktraleng <sopheaktraleng4@gmail.com>
2025-07-21 18:22:39 +0700

    update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,10 +5,8 @@ const Projects = () => {
     return (
         <section className="py-16">
             <div className="max-w-6xl mx-auto px-6"></div>
-            <h2 className="text-4xl font-bold text-foreground mb-4">
-                Featured Projects
-            </h2>
-            <p className="text-lg text-muted-foreground w-96 md:w-auto mx-auto text-center">
+            <h2 className="text-4xl font-bold mb-4">Featured Projects</h2>
+            <p className="text-lg w-96 md:w-auto mx-auto text-center">
                 Building digital solutions and continuously learning through
                 practical experience and academic excellence.
             </p>
